ea925847ff9f8113ef76ee2119fde6183721117d
mauriciocrojas <mauriciocc.rojas@gmail.com>
2024-04-27 18:42:16 -0300

    ejercicio17

--- Prog/Clase02/ejercicio17-auto.php
+++ Prog/Clase02/ejercicio17-auto.php
@@ -9,6 +9,11 @@ class Auto
     private $_marca;
     private $_fecha;
 
+
+    public function getPrecio(){
+        return $this->_precio;
+    }
+
     public function __construct($color, $marca, $precio = 0, $fecha = "Sin especificar")
     {
         $this->_color = $color;
@@ -26,17 +31,59 @@ class Auto
     public static function MostrarAuto(Auto $auto)
     {
         return "Marca: " . $auto->_marca . "<br>" .
-            "Precio: " . $auto->_precio . "<br>" .
+            "Precio: $" . $auto->_precio . "<br>" .
             "Color: " . $auto->_color . "<br>" .
             "Fecha: " . $auto->_fecha . "<br><br>";
     }
 
-    public function Equals(Auto $auto01, Auto $auto02)
+    // public function Equals(Auto $auto01, Auto $auto02)
+    // {
+    //     if ($auto01->_marca == $auto02->_marca) {
+    //         return true;
+    //     }
+    //     return false;
+    // }
+
+
+    //Modificada para que tenga un solo parametro
+    public function Equals(Auto $auto)
     {
-        //Modifica para que tenga un solo parametro
-        if ($auto01->_marca == $auto02->_marca) {
+        if ($this->_marca == $auto->_marca) {
             return true;
         }
         return false;
     }
+
+
+
+    // Crear un método de clase, llamado “Add” que permita sumar dos objetos “Auto” (sólo si son
+    // de la misma marca, y del mismo color, de lo contrario informarlo) y que retorne un Double con
+    // la suma de los precios o cero si no se pudo realizar la operación.
+    // Ejemplo: $importeDouble = Auto::Add($autoUno, $autoDos);
+    public static function Add (Auto $auto01, Auto $auto02){
+        if ($auto01->Equals($auto02) and $auto01->_color == $auto02->_color) {
+            $valor = $auto01->_precio + $auto02->_precio;
+            return $valor;
+            // echo "La suma del valor de ambos autos es de : ". $auto01->_precio. " + 
+            // ". $auto02->_precio. " es de: ". $valor. "<br>";
+        } else {
+            return 0;
+            // echo "No se pudo realizar la suma debido a que los autos no son de la misma marca o color<br>";
+        }
+                    
+        }
+
+        //Utilizar el método de clase “MostrarAuto” para mostrar cada los objetos impares (1, 3, 5)
+        //Mostrar autos impares
+        public static function MostrarImpares (array $autos){
+            foreach ($autos as $key => $value) {
+                echo "<br>Clave: ". $key ." y valor: ". $value ."<br>";
+        }
+    }
+
+
+
+
+
+
 }
--- Prog/Clase02/ejercicio17.php
+++ Prog/Clase02/ejercicio17.php
@@ -17,13 +17,10 @@ iii. La marca, color, precio y fecha.
 
 Realizar un método de instancia llamado “AgregarImpuestos”, que recibirá un doble por
 parámetro y que se sumará al precio del objeto.
-
 Realizar un método de clase llamado “MostrarAuto”, que recibirá un objeto de tipo “Auto”
 por parámetro y que mostrará todos los atributos de dicho objeto.
-
 Crear el método de instancia “Equals” que permita comparar dos objetos de tipo “Auto”. Sólo
 devolverá TRUE si ambos “Autos” son de la misma marca.
-
 Crear un método de clase, llamado “Add” que permita sumar dos objetos “Auto” (sólo si son
 de la misma marca, y del mismo color, de lo contrario informarlo) y que retorne un Double con
 la suma de los precios o cero si no se pudo realizar la operación.
@@ -49,13 +46,59 @@ require_once "ejercicio17-auto.php";
 
 $auto01 = new Auto("Negro", "Ford", 1700, date("d-m-y"));
 $auto02 = new Auto("Blanco", "Ford", 2100);
-$auto03 = new Auto("Verde", "Chevrolet", 1500);
+$auto03 = new Auto("Negro", "Ford", 1500);
 $auto04 = new Auto("Amarillo", "Toyota", 2400);
 
+//Mostramos los vehículos existentes
+echo "Mostramos los vehículos existentes:<br>";
 echo Auto::MostrarAuto($auto01);
+echo Auto::MostrarAuto($auto02);
+echo Auto::MostrarAuto($auto03);
+echo Auto::MostrarAuto($auto04);
 
-if ($auto01->Equals($auto01, $auto02)) {
+
+
+//Agregamos impuestos a los precios de los vehículos
+$auto01->AgregarImpuestos(300);
+$auto02->AgregarImpuestos(900);
+$auto03->AgregarImpuestos(400);
+$auto04->AgregarImpuestos(300);
+
+//Mostramos nuevamente los autos con sus valores actualizados
+echo "<br>Agregamos impuestos a los precios de los vehículos y mostramos sus nuevos valores:<br>";
+echo Auto::MostrarAuto($auto01);
+echo Auto::MostrarAuto($auto02);
+echo Auto::MostrarAuto($auto03);
+echo Auto::MostrarAuto($auto04);
+
+
+//Sumamos el valor de auto01 y auto02 (utilizo getters ya que los atributos son privados)
+echo "Sumamos el valor de auto01 y auto02: $".($auto01->getPrecio()+$auto02->getPrecio())."<br>";
+
+//Sumamos el valor de auto01 y auto03 con función Add:
+$valor01=Auto::Add($auto01,$auto03);
+$valor02=Auto::Add($auto01,$auto04);
+echo "Sumamos con la función Add el valor de auto01 y auto03, y luego del auto01 y auto04;
+ si sus marcas y colores son iguales, retornará la suma, caso contrario 0 <br>
+ Valor 01: $". $valor01."<br>Valor 02: $". $valor02."<br>";
+//Sumamos el valor de auto01 y auto02 con función Add:
+
+
+//Comparamos si son de la misma marca el auto01, y el auto02 y auto04 respectivamente 
+echo "<br>Comparamos si auto01 y auto02 son de la misma marca:<br>";
+if ($auto01->Equals($auto02)) {
     echo "Auto01 y Auto02 son de la misma marca<br>";
 } else {
     echo "Auto01 y Auto02 NO son de la misma marca<br>";
 }
+
+echo "<br>Comparamos si auto01 y auto04 son de la misma marca:<br>";
+if ($auto01->Equals($auto04)) {
+    echo "Auto01 y Auto02 son de la misma marca<br>";
+} else {
+    echo "Auto01 y Auto04 NO son de la misma marca<br>";
+}
+
+$autos = array($auto01, $auto02, $auto03, $auto04);
+
+//Auto::MostrarImpares($autos);
